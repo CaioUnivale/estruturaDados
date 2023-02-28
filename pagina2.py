1deb50f047eb3528f0638acd987747cc1610ef40
CaioUnivale <estudos151927@gmail.com>
2023-02-27 22:11:27 -0300

    Atividade

--- pagina2.py
+++ pagina2.py
@@ -1,2 +1,18 @@
-#Aula Univale - Boa noite alunos
-print (1+1)
+# Aula Univale - Boa noite alunos
+print ("1+1", "Hello", "one", sep=", ")     
+print (22,33,44, sep=", ")
+print ('string')
+print ( type(30 == 20))
+nome1 = 'caio'
+print (type(nome1))
+print(11,1.1, True)
+
+nome = 'Caio Dultra'
+idade = 2002
+print(str(idade) + " anos")
+print(idade, "anos")
+
+myName = 'Caio Dultra'
+myIdade = 20
+altura = 1.67
+peso = '68KG'
